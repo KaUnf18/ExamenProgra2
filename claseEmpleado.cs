1d1fd07fff14c4145242f0955e1aedb0069277e1
KaUnf18 <145079280+KaUnf18@users.noreply.github.com>
2023-10-23 22:31:55 -0600

    Add files via upload

---
+++ claseEmpleado.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace claseEmpleado
+{
+    public class Empleado
+    {
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public double Salario { get; set; }
+
+        public Empleado(string cedula, string nombre, string direccion, string telefono, double salario)
+        {
+            Cedula = cedula;
+            Nombre = nombre;
+            Direccion = direccion;
+            Telefono = telefono;
+            Salario = salario;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la información del empleado:");
+            Console.Write("Cedula: ");
+            string cedula = Console.ReadLine();
+            Console.Write("Nombre: ");
+            string nombre = Console.ReadLine();
+            Console.Write("Dirección: ");
+            string direccion = Console.ReadLine();
+            Console.Write("Teléfono: ");
+            string telefono = Console.ReadLine();
+            Console.Write("Salario: ");
+            double salario;
+            double.TryParse(Console.ReadLine(), out salario);
+
+            Empleado empleado = new Empleado(cedula, nombre, direccion, telefono, salario);
+
+            Console.WriteLine("Información del empleado:");
+            Console.WriteLine($"Cedula: {empleado.Cedula}");
+            Console.WriteLine($"Nombre: {empleado.Nombre}");
+            Console.WriteLine($"Dirección: {empleado.Direccion}");
+            Console.WriteLine($"Teléfono: {empleado.Telefono}");
+            Console.WriteLine($"Salario: {empleado.Salario}");
+        }
+    }
+}
+
